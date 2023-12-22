061976bb85487592f1154333f2b30d2b4e77dabf
Ishizaka Ryo <riririrrr@icloud.com>
2023-12-23 00:45:18 +0900

    セットアップ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,126 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  interface Player {
+    id: number;
+    name: string;
+    position: number;
+  }
+  const battingNums = [
+    { order: "firstHitter" },
+    { order: "secoundHitter" },
+    { order: "thirdHitter" },
+    { order: "fourthHitter" },
+    { order: "fifthHitter" },
+    { order: "sixthHitter" },
+    { order: "seventhHitter" },
+    { order: "eightHitter" },
+    { order: "ninthHitter" },
+  ];
+  const positions = [
+    { positionName: "捕", positionNum: 2 },
+    { positionName: "一", positionNum: 3 },
+    { positionName: "二", positionNum: 4 },
+    { positionName: "三", positionNum: 5 },
+    { positionName: "遊", positionNum: 6 },
+    { positionName: "左", positionNum: 7 },
+    { positionName: "中", positionNum: 8 },
+    { positionName: "右", positionNum: 9 },
+    { positionName: "指", positionNum: 10 },
+  ];
+  const players: Player[] = [
+    { id: 1, name: "山本由伸", position: 1 },
+    { id: 2, name: "宮城大弥", position: 1 },
+    { id: 3, name: "山岡泰輔", position: 1 },
+    { id: 4, name: "山下舜平太", position: 1 },
+    { id: 5, name: "若月健矢", position: 2 },
+    { id: 6, name: "森友哉", position: 2 },
+    { id: 6, name: "頓宮裕真", position: 3 },
+    { id: 6, name: "セデーニョ", position: 3 },
+    { id: 6, name: "安達了一", position: 4 },
+    { id: 6, name: "宜保翔", position: 4 },
+    { id: 6, name: "宗佑磨", position: 5 },
+    { id: 6, name: "西野真弘", position: 5 },
+    { id: 6, name: "紅林弘太郎", position: 6 },
+    { id: 6, name: "野口智哉", position: 6 },
+    { id: 6, name: "福田周平", position: 7 },
+    { id: 6, name: "佐野皓大", position: 7 },
+    { id: 6, name: "中川圭太", position: 8 },
+    { id: 6, name: "茶野篤政", position: 8 },
+    { id: 6, name: "杉本裕太郎", position: 9 },
+    { id: 6, name: "小田裕也", position: 9 },
+  ];
+
+  const batters = players.filter((player) => {
+    return player.position !== 1;
+  });
+  const pitchers = players.filter((player) => {
+    return player.position === 1;
+  });
+
+  const [battingOrder, setBattingOrder] = useState({});
+  const [positionOrder, setPositionOrder] = useState({});
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("TEST");
+  };
+
+  const changeBattingOrder = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setBattingOrder({ ...battingOrder, [e.target.name]: e.target.value });
+  };
+  const changePositionOrder = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setPositionOrder({ ...positionOrder, [e.target.name]: e.target.value });
+    console.log(positionOrder);
+  };
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main>
+      <div>
+        <p>テスト</p>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          {battingNums.map((battingNum, key) => {
+            return (
+              <div>
+                <a href="">{key + 1}</a>
+                <select
+                  name={battingNum.order}
+                  onChange={(e) => changeBattingOrder(e)}
+                >
+                  <option value="">選択してください</option>
+                  {batters.map((batter) => {
+                    return <option value={batter.name}>{batter.name}</option>;
+                  })}
+                </select>
+                <select
+                  name={battingNum.order}
+                  onChange={(e) => changePositionOrder(e)}
+                >
+                  <option value="">未選択</option>
+                  {positions.map((position) => {
+                    return (
+                      <option value={position.positionNum}>
+                        {position.positionName}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+            );
+          })}
+          <div>
+            <select name="pitcher">
+              {pitchers.map((pitcher) => {
+                return <option value={pitcher.id}>{pitcher.name}</option>;
+              })}
+            </select>
+          </div>
+          <input type="submit" value="送信" />
+        </form>
       </div>
     </main>
-  )
+  );
 }
